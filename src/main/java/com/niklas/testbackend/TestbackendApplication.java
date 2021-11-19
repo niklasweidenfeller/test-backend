4ad3984aa86e554480afdb0831d00a3c8d8cd1a1
niklasweidenfeller <niklas.weidenfeller@gmx.de>
2021-11-19 13:38:17 +0100

    allow CORS requests

--- src/main/java/com/niklas/testbackend/TestbackendApplication.java
+++ src/main/java/com/niklas/testbackend/TestbackendApplication.java
@@ -3,11 +3,29 @@ package com.niklas.testbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class TestbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestbackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(
+					"http://localhost:3000",
+					"https://testprojectniklas-frontend.herokuapp.com"
+				);
+			}
+		};
 	}
 
 }
