b229093b8f84bf788eb266cb952513040c1b8790
niklasweidenfeller <niklas.weidenfeller@gmx.de>
2021-11-19 13:10:34 +0100

    initial commit

---
+++ src/test/java/com/niklas/testbackend/TestbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niklas.testbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
